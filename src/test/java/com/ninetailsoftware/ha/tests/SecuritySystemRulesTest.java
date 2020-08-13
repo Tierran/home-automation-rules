1279cfe5b00ad0dd0f5d06e39e6f476514ed9c07
David Tucker <drgnfox@gmail.com>
2020-08-13 13:40:33 -0500

    Updating rules to allow updating system status from email route

--- src/test/java/com/ninetailsoftware/ha/tests/SecuritySystemRulesTest.java
+++ src/test/java/com/ninetailsoftware/ha/tests/SecuritySystemRulesTest.java
@@ -31,6 +31,26 @@ public class SecuritySystemRulesTest extends MyTestHelper {
 	}
 	
 	@Test
+	public void testAlarmArmedAwayByEmail() throws Exception {
+		KieSession ksession = testHelper("Update Device Status.drl", "Security System Rules.drl");
+		
+		HaEvent ha = new HaEvent();
+		ha.setValue("System Armed (away mode)");
+		ha.setDeviceId("216");
+		
+		AlarmPanel panel = new AlarmPanel();
+		panel.setId("216");
+		panel.setStatus("0");
+		panel.setSource("homeseer");
+		
+		ksession.insert(ha);
+		ksession.insert(panel);
+		ksession.fireAllRules();
+		
+		Assert.assertEquals("Check that alarm panel was armed away (Status 2)", "2", panel.getStatus());
+	}
+	
+	@Test
 	public void testAlarmArmedStay() throws Exception {
 		KieSession ksession = testHelper("Update Device Status.drl", "Security System Rules.drl");
 		
@@ -51,12 +71,50 @@ public class SecuritySystemRulesTest extends MyTestHelper {
 	}
 	
 	@Test
+	public void testAlarmArmedStayByEmail() throws Exception {
+		KieSession ksession = testHelper("Update Device Status.drl", "Security System Rules.drl");
+		
+		HaEvent ha = new HaEvent();
+		ha.setValue("System Armed (home mode)");
+		
+		AlarmPanel panel = new AlarmPanel();
+		panel.setId("216");
+		panel.setStatus("0");
+		panel.setSource("homeseer");
+		
+		ksession.insert(ha);
+		ksession.insert(panel);
+		ksession.fireAllRules();
+		
+		Assert.assertEquals("Check that alarm panel was armed away (Status 1)", "1", panel.getStatus());
+	}
+	
+	@Test
 	public void testAlarmDisarmed() throws Exception {
 		KieSession ksession = testHelper("Update Device Status.drl", "Security System Rules.drl");
 		
 		HaEvent ha = new HaEvent();
 		ha.setValue("0");
 		ha.setDeviceId("216");
+		
+		AlarmPanel panel = new AlarmPanel();
+		panel.setId("216");
+		panel.setStatus("2");
+		panel.setSource("homeseer");
+		
+		ksession.insert(ha);
+		ksession.insert(panel);
+		ksession.fireAllRules();
+		
+		Assert.assertEquals("Check that alarm panel was armed away (Status 0)", "0", panel.getStatus());
+	}
+	
+	@Test
+	public void testAlarmDisarmedByEmail() throws Exception {
+		KieSession ksession = testHelper("Update Device Status.drl", "Security System Rules.drl");
+		
+		HaEvent ha = new HaEvent();
+		ha.setValue("System Disarmed");
 		
 		AlarmPanel panel = new AlarmPanel();
 		panel.setId("216");
